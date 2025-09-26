07bd7b7e0bfed32a348884db0a5bef90a591ba53
unfazedXviraj <virajvarma039@gmail.com>
2025-09-26 12:13:01 +0530

    Create day2q4.c

---
+++ day2q4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    float radii,area,circle;
+
+    printf("The radius of the circle is:");
+    scanf("%f",&radii);
+    
+    area=3.14*radii*radii;
+    printf("The area of the circle is %f sq units \n", area);
+    circle=2*3.14*radii;
+    printf("The perimeter of the circle is %f sq units", circle);
+    
+    return 0;
+
+}
