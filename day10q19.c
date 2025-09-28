24728fd818ec47fb9e3172d41194b35ab45f876c
unfazedXviraj <virajvarma039@gmail.com>
2025-09-29 01:07:48 +0530

    Create day10q19.c

---
+++ day10q19.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+
+    printf("Enter three sides of the triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if ((a + b > c) && (a + c > b) && (b + c > a))
+    {
+        if (a == b && b == c)
+        {
+            printf("The triangle is Equilateral.\n");
+        }
+        else if (a == b || b == c || a == c)
+        {
+            printf("The triangle is Isosceles.\n");
+        }
+        else
+        {
+            printf("The triangle is Scalene.\n");
+        }
+    }
+    else
+    {
+        printf("The given sides do not form a valid triangle.\n");
+    }
+
+    return 0;
+}
