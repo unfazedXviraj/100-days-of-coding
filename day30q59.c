520522031ae7a7a914f283dfc5632f86cf3c386d
unfazedXviraj <virajvarma039@gmail.com>
2025-10-01 00:21:30 +0530

    Create day30q59.c

---
+++ day30q59.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int arr[100];
+    int evenCount = 0, oddCount = 0;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0)
+            evenCount++;
+        else
+            oddCount++;
+    }
+    printf("Total even numbers = %d\n", evenCount);
+    printf("Total odd numbers  = %d\n", oddCount);
+
+    return 0;
+}
