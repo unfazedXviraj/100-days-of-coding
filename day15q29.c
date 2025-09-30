0212f1d015c427e361f128deb5d8f584145cc237
unfazedXviraj <virajvarma039@gmail.com>
2025-09-30 22:02:21 +0530

    Create day15q29.c

---
+++ day15q29.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter a non-negative integer: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+
+    printf("Factorial of %d = %d\n", n, factorial);
+
+    return 0;
+}
