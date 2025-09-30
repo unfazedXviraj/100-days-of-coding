ba035f5695576dacf66c8475f7cca1a4527682ea
unfazedXviraj <virajvarma039@gmail.com>
2025-09-30 21:48:12 +0530

    Create day13q25.c

---
+++ day13q25.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2, result;
+
+    printf("Enter an operator (+, -, *, /, %%): ");
+    scanf(" %c", &operator);
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch(operator) {
+        case '+':
+            result = num1 + num2;
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, result);
+            break;
+            
+        case '-':
+            result = num1 - num2;
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, result);
+            break;
+            
+        case '*':
+            result = num1 * num2;
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, result);
+            break;
+            
+        case '/':
+            if(num2 != 0) {
+                result = num1 / num2;
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, result);
+            } else {
+                printf("Division by zero is not allowed.\n");
+            }
+            break;
+            
+        case '%':
+            if((int)num2 != 0)
+            {
+                result = (int)num1 % (int)num2;
+                printf("%d%d%d = %.0lf\n", (int)num1, (int)num2, result);
+            }
+            else
+            {
+                printf(Division by zero is not allowed.\n");
+            }
+
+    return 0;
+}
