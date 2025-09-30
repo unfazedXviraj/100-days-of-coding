a76e39ef2d85a60d8594558701a81fcceafd2544
unfazedXviraj <virajvarma039@gmail.com>
2025-09-30 21:54:40 +0530

    Create day14q27.c

---
+++ day14q27.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0, odd = 1;
+
+    printf("Enter a positive integer n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++)
+    {
+        sum += odd;
+        odd += 2;
+    }
+    printf("The sum of first %d odd numbers is: %d\n", n, sum);
+
+    return 0;
+}
