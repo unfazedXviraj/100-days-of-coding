c202190e1bcddc43b9dd0da4092484e31e909281
unfazedXviraj <virajvarma039@gmail.com>
2025-09-30 22:18:36 +0530

    Create day17q33.c

---
+++ day17q33.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num, original, sum = 0, digits = 0, temp;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    original = num;
+    temp = num;
+
+    // Count the number of digits
+    while (temp != 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    temp = num;
+    // Calculate sum of each digit raised to the power of 'digits'
+    while (temp != 0) {
+        int digit = temp % 10;
+        sum += pow(digit, digits);
+        temp /= 10;
+    }
+
+    if (sum == original)
+        printf("%d is an Armstrong number.\n", original);
+    else
+        printf("%d is not an Armstrong number.\n", original);
+
+    return 0;
+}
