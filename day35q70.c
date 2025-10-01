1e41839968fbca508b35d757b419c5779340f469
unfazedXviraj <virajvarma039@gmail.com>
2025-10-01 14:18:26 +0530

    Create day35q70.c

---
+++ day35q70.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, largest, second_largest;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    if (n < 2) {
+        printf("Array must have at least 2 elements.\n");
+        return 1;
+    }
+
+    int arr[100];
+    printf("Enter %d elements: ", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    if (arr[0] > arr[1]) {
+        largest = arr[0];
+        second_largest = arr[1];
+    } else {
+        largest = arr[1];
+        second_largest = arr[0];
+    }
+    for (i = 2; i < n; i++) {
+        if (arr[i] > largest) {
+            second_largest = largest;
+            largest = arr[i];
+        } else if (arr[i] > second_largest && arr[i] != largest) {
+            second_largest = arr[i];
+        }
+    }
+
+    printf("Second largest element: %d\n", second_largest);
+
+    return 0;
+}
