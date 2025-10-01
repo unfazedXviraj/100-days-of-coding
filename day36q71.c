46b4479b2b596ee390b5be41400974e445cad01f
Viraj Varma <virajvarma039@gmail.com>
2025-10-01 14:37:32 +0530

    Create day36q71.c

---
+++ day36q71.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+  
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    // Input matrix elements
+    printf("Enter elements of the matrix:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("Element [%d][%d]: ", i, j);
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Print the matrix
+    printf("The matrix is:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
