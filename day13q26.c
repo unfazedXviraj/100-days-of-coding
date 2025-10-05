7b9b00390cc7d72f311578e97cb745ea6759fe4f
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:33:15 +0530

    day13q26.c

--- day13q26.c
+++ day13q26.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-
 int main() {
     int n, i;
-
     printf("Enter a positive integer n: ");
     scanf("%d", &n);
-
     if (n <= 0) {
         printf("negtive integer\n");
         return 1;
     }
-
     printf("Numbers from 1 to %d are:\n", n);
     for (i = 1; i <= n; i++) {
         printf("%d ", i);
     }
     printf("\n");
-
     return 0;
 }
