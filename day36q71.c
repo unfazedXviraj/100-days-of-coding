4a32f2c165ec6f04c2c5aef78dbe593305a3512d
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:59:15 +0530

    day36q71.c

--- day36q71.c
+++ day36q71.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 int main() {
     int rows, cols;
-  
     printf("Enter number of rows: ");
     scanf("%d", &rows);
     printf("Enter number of columns: ");
     scanf("%d", &cols);
-
     int matrix[rows][cols];
-
-    // Input matrix elements
     printf("Enter elements of the matrix:\n");
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
@@ -18,8 +13,6 @@ int main() {
             scanf("%d", &matrix[i][j]);
         }
     }
-
-    // Print the matrix
     printf("The matrix is:\n");
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
@@ -27,6 +20,5 @@ int main() {
         }
         printf("\n");
     }
-
     return 0;
 }
