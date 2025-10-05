7b6d5e00276e401d16463866e307e6bea6e8c688
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:32:23 +0530

    day10q19.c

--- day10q19.c
+++ day10q19.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-
 int main() {
     int a, b, c;
-
     printf("Enter three sides of the triangle: ");
     scanf("%d %d %d", &a, &b, &c);
-
     if ((a + b > c) && (a + c > b) && (b + c > a))
     {
         if (a == b && b == c)
@@ -25,6 +22,5 @@ int main() {
     {
         printf("The given sides do not form a valid triangle.\n");
     }
-
     return 0;
 }
