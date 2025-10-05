79d7b1c45d99d283f47c2f46b9fd4bd57808bf70
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 18:00:22 +0530

    Update day37q72.c

--- day37q72.c
+++ day37q72.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
-
 int main() {
     int rows, cols, sum = 0;
-
     printf("Enter number of rows: ");
     scanf("%d", &rows);
     printf("Enter number of columns: ");
     scanf("%d", &cols);
-
     int matrix[rows][cols];
-
-    // Input matrix elements
     printf("Enter elements of the matrix:\n");
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             scanf("%d", &matrix[i][j]);
-            sum += matrix[i][j]; // Add element to sum
+            sum += matrix[i][j]; 
         }
     }
-
     printf("Sum of all elements in the matrix: %d\n", sum);
-
     return 0;
 }
