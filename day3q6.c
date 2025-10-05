7399663eb69e6d278a9e91f018025d7a91e1b3e3
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 18:02:33 +0530

    day3q6.c

--- day3q6.c
+++ day3q6.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-
 int main()
 {
     int a, b, temp;
-
     printf("Enter value of a: ");
     scanf("%d", &a);
-
     printf("Enter value of b: ");
-    scanf("%d", &b);
-    
+    scanf("%d", &b); 
     temp = a;
     a = b;
     b = temp;
-
     printf("After swapping:\n");
     printf("a = %d\n", a);
     printf("b = %d\n", b);
-
     return 0;
 }
