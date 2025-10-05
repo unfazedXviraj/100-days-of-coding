fddd092d3cffb3d62b2967333db936efed504a5e
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:54:08 +0530

    day32q64.c

--- day32q64.c
+++ day32q64.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-
 int main() {
     long long num;
     int freq[10] = {0}; 
     int digit, i, maxDigit = 0, maxFreq = 0;
-
     printf("Enter an integer number: ");
     scanf("%lld", &num);
-
     if (num < 0) {
         num = -num;
     }
-
     while (num > 0) {
         digit = num % 10;
         freq[digit]++;
@@ -23,8 +19,6 @@ int main() {
             maxDigit = i;
         }
     }
-
     printf("Digit that occurs most is %d (appears %d times)\n", maxDigit, maxFreq);
-
     return 0;
 }
