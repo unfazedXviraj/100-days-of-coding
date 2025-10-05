1768535936f99cf353502f8578601dedd6fa84d6
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:37:12 +0530

    day18q35.c

--- day18q35.c
+++ day18q35.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 int main()
 {
     int num, i;
-
     printf("Enter a number: ");
     scanf("%d", &num);
-
     printf("Factors of %d are: ", num);
     for(i = 1; i <= num; i++) 
     {
@@ -16,6 +13,5 @@ int main()
         }
     }
     printf("\n");
-
     return 0;
 }
