587de3d9357133ad02b3ade936495c29a5b7039d
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:36:47 +0530

    day17q34.c

--- day17q34.c
+++ day17q34.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
-
 int main()
 {
-    int num, i, isPrime = 1; // Assume prime
-
+    int num, i, isPrime = 1; 
     printf("Enter an integer: ");
     scanf("%d", &num);
 
     if (num <= 1)
-        isPrime = 0; // 0 and 1 are not prime numbers
+        isPrime = 0; 
     else {
         for (i = 2; i * i <= num; i++) {
             if (num % i == 0) {
@@ -17,11 +15,9 @@ int main()
             }
         }
     }
-
     if (isPrime)
         printf("%d is a prime number.\n", num);
     else
         printf("%d is not a prime number.\n", num);
-
     return 0;
 }
