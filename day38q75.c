58b76af122b654af0d3ed87ba7dde0bb1532dde5
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 18:01:07 +0530

    Update day38q75.c

--- day38q75.c
+++ day38q75.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
-
 int main() {
     int rows, cols;
     printf("Enter number of rows: ");
     scanf("%d", &rows);
     printf("Enter number of columns: ");
     scanf("%d", &cols);
-
     int a[rows][cols], b[rows][cols], sum[rows][cols];
-
-    // Input first matrix
     printf("Enter elements of first matrix:\n");
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             scanf("%d", &a[i][j]);
         }
     }
-
-    // Input second matrix
     printf("Enter elements of second matrix:\n");
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             scanf("%d", &b[i][j]);
         }
     }
-
-    // Add matrices
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
             sum[i][j] = a[i][j] + b[i][j];
         }
     }
-
-    // Print result
     printf("Sum of the matrices:\n");
     for(int i = 0; i < rows; i++) {
         for(int j = 0; j < cols; j++) {
@@ -40,6 +30,5 @@ int main() {
         }
         printf("\n");
     }
-
     return 0;
 }
