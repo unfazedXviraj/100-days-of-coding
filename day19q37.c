2323c2f5b033bbefe7d2226367b914ac32340821
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:38:22 +0530

    day19q37.c

--- day19q37.c
+++ day19q37.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 int main()
 {
     int a, b, max, lcm;
-
     printf("Enter two numbers: ");
     scanf("%d %d", &a);
     max = (a > b) ? a : b;
-
     while(1) {
         if(max % a == 0 && max % b == 0) {
             lcm = max;
@@ -15,8 +12,6 @@ int main()
         }
         max++;
     }
-
     printf("LCM of %d and %d is %d\n", a, b, lcm);
-
     return 0;
 }
