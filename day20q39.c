446b3d53e156e3653128f77bd3989407cd5e9f61
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:40:19 +0530

    day20q39.c

--- day20q39.c
+++ day20q39.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
-
 int main()
 {
     int num, digit, product = 1, hasOdd = 0;
-
     printf("Enter a number: ");
     scanf("%d", &num);
-
-    // Handle negative numbers
     if(num < 0)
         num = -num;
-
     if (num == 0) {
         printf("Product of odd digits is: 0\n");
         return 0;
     }
-
     while(num != 0) {
         digit = num % 10;
         if(digit % 2 != 0) {
@@ -24,11 +18,9 @@ int main()
         }
         num /= 10;
     }
-
     if(hasOdd)
         printf("Product of odd digits is: %d\n", product);
     else
         printf("No odd digits found. Product is 0.\n");
-
     return 0;
 }
