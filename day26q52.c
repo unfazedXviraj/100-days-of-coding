77f0d9669aa12f32e3d600ed945d18470593f424
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:46:00 +0530

    day26q52.c

--- day26q52.c
+++ day26q52.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
-
 int main() {
     printf("*\n");
-    printf("\n");  
-  
+    printf("\n");   
     for (int i = 0; i < 4; i++) {
         printf("*\n");
     }
     printf("\n");  
-  
     for (int i = 0; i < 5; i++) {
         printf("*\n");
     }
     printf("\n");  
-  
     for (int i = 0; i < 3; i++) {
         printf("*\n");
     }
     printf("\n");  
-
-    printf("*\n");
-    
+    printf("*\n");    
     return 0;
 }
