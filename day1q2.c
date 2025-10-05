2e30e34826a7104d842b3da6b9a8ecb566d2c5dc
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:39:40 +0530

    day1q2.c

--- day1q2.c
+++ day1q2.c
@@ -1,28 +1,16 @@
 #include<stdio.h>
-
-void main(){
+void main()
+{
     int x,y;
     printf("Enter 1st number:");
     scanf("%d",&x);
-
     printf("Enter 2nd number:");
     scanf("%d",&y);
-
-
-    printf("The sum of the numbers is:%d \n",x+y); // addition
-
-  
-    printf("The difference of the numbers is:%d \n",x-y); // subtraction
- 
-    
-    printf("The product of the numbers is:%d \n",x*y); // multiplication
-
-    // quotient 
+    printf("The sum of the numbers is:%d \n",x+y);   
+    printf("The difference of the numbers is:%d \n",x-y); 
+    printf("The product of the numbers is:%d \n",x*y); 
     if (y==0)
     printf("The quotient of the numbers is: Indefinite \n"); 
     else 
     printf("The quotient of the numbers is:%d \n", x/y);
-
-    
-
 }
