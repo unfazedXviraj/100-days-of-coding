48a2c6f7337da41a344814867803d5cef1958ec6
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:41:35 +0530

    day21q42.c

--- day21q42.c
+++ day21q42.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
-
 int main()
 {
     int num, sum = 0, i;
-
     printf("Enter a number: ");
     scanf("%d", &num);
-
     for(i = 1; i < num; i++) {
         if(num % i == 0) {
             sum += i;
         }
     }
-
     if(sum == num && num != 0) {
         printf("%d is a perfect number.\n", num);
     } else {
         printf("%d is not a perfect number.\n", num);
     }
-
     return 0;
 }
