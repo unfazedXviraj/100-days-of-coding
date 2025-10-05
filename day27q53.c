54b8df882f22d79f894f35614c0b55d18f8fac8d
Viraj Varma <virajvarma039@gmail.com>
2025-10-05 17:46:41 +0530

    day27q53.c

--- day27q53.c
+++ day27q53.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
-
 int main() {
-    int n = 5;
-  
-    for (int i = 1; i <= n; i++) {
-    
+    int n = 5;  
+    for (int i = 1; i <= n; i++) {    
         for (int j = 1; j <= 2*i - 1; j++) {
             printf("*");
         }
         printf("\n");
     }
-
     for (int i = n - 1; i >= 1; i--) {
 
         for (int j = 1; j <= 2*i - 1; j++) {
@@ -18,6 +14,5 @@ int main() {
         }
         printf("\n");
     }
-    
     return 0;
 }
